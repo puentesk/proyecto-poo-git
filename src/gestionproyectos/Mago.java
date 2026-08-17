37375668c82aba5d9bce43ff2be29f4bed3d8f93
Kevin Puentes <puentesvasquezk@gmail.com>
2026-08-17 17:14:05 -0500

    feat:agrega estructura base de Mago y metodo lanzarHechizo

---
+++ src/gestionproyectos/Mago.java
@@ -0,0 +1,14 @@
+package gestionproyectos;
+
+public class Mago extends Tarea {
+    private int mana;
+
+    public Mago(String titulo, String descripcion, int mana) {
+        super(titulo, descripcion);
+        this.mana = mana;
+    }
+
+    public void lanzarHechizo() {
+        System.out.println("Lanzando hechizo...");
+    }
+}
