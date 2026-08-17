6953b4944a189d9ca783f53c4b4a4cea50237a20
Kevin Puentes <puentesvasquezk@gmail.com>
2026-08-17 15:30:22 -0500

    feat: agrega constructor clase Tarea

--- src/gestionproyectos/Tarea.java
+++ src/gestionproyectos/Tarea.java
@@ -1,3 +1,13 @@
 public class Tarea {
     
 }
+
+public class Tarea {
+    private String titulo;
+    private String descripcion;
+
+    public Tarea(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+}
